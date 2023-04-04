9662ab014bc9aeab0082f7af6e3b8482d2645746
am999 <am9999072080@gmail.com>
2023-04-05 01:16:08 +0300

    курсовая работа

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,78 @@
 public class Employee {
+    private String name;
+    private String surname;
+    private String middleName;
+
+    private String department;
+    private int salary;
+
+    private int id;
+    static int counter = 1;
+
+    public void displayId() {
+        id = counter++;
+
+    }
+
+    public Employee(String surname, String name, String middleName, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setMiddleSalary(int salary) {
+        this.salary = salary;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return id
+                + surname + " "
+                + name + " "
+                + middleName + " "
+                + department + " "
+                + salary + " $";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        Employee employee1 = new Employee(" Иванов      ", " Иван      ", " Иванович      ", " Специалист по сетевому обеспечению ", 2_000);
+        Employee employee2 = new Employee(" Сергеев     ", " Сергей    ", " Сергеевич     ", " Системный аналитик                 ", 2_200);
+        Employee employee3 = new Employee(" Алексеев    ", " Алексей   ", " Алексеевич    ", " Программист                        ", 2_500);
+        Employee employee4 = new Employee(" Александров ", " Александр ", " Александрович ", " Обеспечение работоспособности в IT ", 2_300);
+        Employee employee5 = new Employee(" Иванов      ", " Иван      ", " Иванович      ", " Системный администратор            ", 2_400);
 
+        employee1.displayId();    // Id = 1
+        employee2.displayId();    // Id = 2
+        employee3.displayId();    // Id = 3
+        employee4.displayId();    // Id = 4
+        employee5.displayId();    // Id = 5
+
+        Employee[] vaults = new Employee[10];
+        vaults[0] = employee1;
+        vaults[1] = employee2;
+        vaults[2] = employee3;
+        vaults[3] = employee4;
+        vaults[4] = employee5;
+
+        System.out.println(vaults[0]);
+        System.out.println(vaults[1]);
+        System.out.println(vaults[2]);
+        System.out.println(vaults[3]);
+        System.out.println(vaults[4]);
+
+        int[] arr = {employee1.getSalary(), employee2.getSalary(), employee3.getSalary(), employee4.getSalary(), employee5.getSalary(),};
+        int totalAmount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalAmount += arr[i];
+        }
+        System.out.println("Общая сумма затрат= " + totalAmount + " $");
+
+        int maximum = -1;
+        int minimum = arr[0];
+        Arrays.sort(arr);
+        for (int i : arr) {
+            maximum = i;
+            minimum = arr[0];
+        }
+        System.out.println("Максимальная зарплата= " + maximum + " $");
+        System.out.println("Минимальной зарплата= " + minimum + " $");
+
+        double average;
+        double sum = Arrays.stream(arr).sum();
+
+        average = sum / arr.length;
+        System.out.println("Средняя зарплата= " + average + " $");
     }
 }
